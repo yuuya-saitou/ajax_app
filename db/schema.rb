1b262f5fd203120716edb01509bbaa4db996c8cb
yuuya-saitou <ktkstyyshaba@gmail.com>
2020-09-02 16:07:45 +0900

    AjaxAppの準備

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2020_08_10_032549) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.text "content"
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
